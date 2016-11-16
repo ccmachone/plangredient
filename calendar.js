6027f42162f66338b6e8062772d7b062650e09b9
Colby Clark <ccmachone@gmail.com>
2016-11-15 20:27:29 -0700

    Implemented function to get a meal plan for week "1" or week "2" (week "1" is "this" week, week "2" is "next" week).

--- calendar.js
+++ calendar.js
@@ -210,3 +210,54 @@ function unPlanAllRecipesOnDateResult(promise)
 {
     console.log(promise);
 }
+
+function getPlanForWeek(week_number, callback)
+{
+    var logged_in_user = getLoggedInUser();
+    if (logged_in_user['id'] == undefined) {
+        return false;
+    } else {
+        var one_day_in_milliseconds = 60 * 60 * 24 * 1000;
+        var week_start_day = new Date();
+        if (week_number == 1) {
+            while (week_start_day.getDay() != 0) {
+                week_start_day = new Date(week_start_day.getTime() - one_day_in_milliseconds);
+            }
+        } else {
+            do {
+                week_start_day = new Date(week_start_day.getTime() + one_day_in_milliseconds);
+            } while (week_start_day.getDay() != 0);
+        }
+        week_start_day.setHours(0);
+        week_start_day.setMinutes(0);
+        week_start_day.setSeconds(0);
+        week_start_day.setMilliseconds(0);
+
+        planned_recipes_ref.orderByChild("email").equalTo(logged_in_user['user']['email']).once("value").then(function(snapshot) {
+            planned_recipes_objects = snapshot.val();
+            var week_plan = {};
+            for (var key in planned_recipes_objects) {
+                if (planned_recipes_objects.hasOwnProperty(key)) {
+                    if (planned_recipes_objects[key]['date'] >= week_start_day.getTime() && planned_recipes_objects[key]['date'] < week_start_day.getTime() + one_day_in_milliseconds * 7) {
+                        var temp_date = new Date(planned_recipes_objects[key]['date']);
+                        if (week_plan[temp_date.getDay()] == undefined) {
+                            week_plan[temp_date.getDay()] = {};
+                        }
+                        week_plan[temp_date.getDay()][planned_recipes_objects[key]['meal']] = planned_recipes_objects[key]['recipe_ids'];
+                    }
+                }
+            }
+            if (callback == undefined) {
+                callback = "getPlanForWeekResult";
+                window[callback](week_plan);
+            } else {
+                callback(week_plan);
+            }
+        })
+    }
+}
+
+function getPlanForWeekResult(week_plan)
+{
+    console.log(week_plan);
+}
