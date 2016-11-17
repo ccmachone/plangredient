f0fd7fade89a4e5b13e50cc1cecad5296300b0d9
Colby Clark <ccmachone@gmail.com>
2016-11-16 21:07:54 -0700

    Recipes tab styling

--- recipes_tab.js
+++ recipes_tab.js
@@ -217,7 +217,7 @@ recipes_ref.on("child_added", function(recipe){
     var html = "<li id='persisted_recipe_" + recipe.key + "'>" + recipe_object['name'] + "</li>";
     jQuery("#existing_recipes").append(html);
 
-    html = "<div class='col-md-4'><a class='thumbnail' href='recipe_display.html?recipe_id=" + recipe.key + "'><img class='recipe_image img-rounded' src='";
+    html = "<div class='col-md-3 clickableRecipe'><div style='cursor: pointer;' onclick='window.location.href=\"recipe_display.html?recipe_id=" + recipe.key + "\"'><img class='recipe_image img-rounded' src='";
     if (recipe_object['image'] != "" && recipe_object['image'] != undefined) {
         html += recipe_object['image'];
     } else {
