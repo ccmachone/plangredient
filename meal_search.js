334ecf2f337e6eed99603aa6402eda618af26870
Jacob Carter <jacobcarter.xc@gmail.com>
2016-11-17 21:53:24 -0700

    filter selection working

---
+++ meal_search.js
@@ -0,0 +1,51 @@
+
+var currentSelectedFilters = [];
+
+
+$( document ).ready(function()
+{
+	
+	$("#searchButton").click(function()
+	{
+		
+		var searchTerm = $("#searchBar").val(); 
+		recipesSearch(searchTerm);
+		
+	});
+
+	$("#filterButton").click(function()
+	{
+		var input = $("#filterBar").val();
+		currentSelectedFilters.push(input);
+		updateFilterDisplay();
+	});
+	
+});
+
+
+
+function updateFilterDisplay()
+{
+	var activeFilters = "<ul>";
+	for(filter in currentSelectedFilters)
+	{
+		activeFilters = activeFilters + "<li>"+currentSelectedFilters[filter]+"<img style = 'height: 10px; width: 10px; margin-left: 5px;' src = 'http://vignette4.wikia.nocookie.net/five-nights-at-tubbyland/images/a/a5/X.png/revision/latest?cb=20160216225020' onclick = 'removeFilter(\""+currentSelectedFilters[filter]+"\")'></img></li>";
+	}
+	$("#selectedFilters").html(activeFilters);
+}
+
+function removeFilter(filter)
+{
+	var index = currentSelectedFilters.indexOf(filter);
+	if (index > -1)
+	{
+		currentSelectedFilters.splice(index, 1);
+	}
+	updateFilterDisplay();
+}
+
+
+function recipesSearch(searchTerm)
+{
+	//planned_recipes_ref.orderByChild("email").equalTo(logged_in_user['user']['email']).once("value").then(function(snapshot) {
+}
