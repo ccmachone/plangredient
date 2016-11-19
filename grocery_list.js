0fedb24831c94958ad04b6844b85461f1f3d204e
Colby Clark <ccmachone@gmail.com>
2016-11-18 19:02:46 -0700

    Dynamically build the grocery list and list of planned meals.

--- grocery_list.js
+++ grocery_list.js
@@ -1,46 +1,77 @@
 jQuery(document).ready(function(){
-	$(".check_del").hide();
-    getGroceries(buildGroceryList);
-    /*
-    $("#print_list").click(function() {
-    	console.log("#grocery_list".innerHTML);
-        printDiv();
-    	//print($(this).parent(".grocery_list"));
-    });
-*/
-    $(".delete").click(function() {
-    	$(this).siblings(".check_del").show();
-    });
-    $(".yes").click(function() {
-        console.log(".check_del".parent);
-        $(this).parent(".check_del").parent().hide();
-    });
+    getGroceries();
+});
 
+jQuery(document).on("click", ".delete", function(){
+    jQuery(this).siblings(".check_del").show();
+});
+
+jQuery(document).on("click", ".yes", function(){
+    jQuery(this).parent(".check_del").parent().hide();
+});
+
+jQuery(document).on("click", ".cancel", function(){
+    jQuery(this).parent().hide();
+})
+
+jQuery(document).on("submit", "grocery_list_items", function(e){
+    e.stopPropagation();
+    e.preventDefault();
 });
 
 
 
-function printDiv() {    
+function printDiv()
+{
     $(".heading").hide();
     $("#print_list").hide();
+    jQuery(".non_printable").hide();
+    jQuery(".printable").show();
+    jQuery(".check_del").hide();
 
-    var printContents = document.getElementById('grocery_list').innerHTML;
-    var originalContents = document.body.innerHTML;
-    
-    //print(printContents);
-    //document.body.innerHTML = printContents;
-    //var prtContent = document.getElementById('grocery_list').ingredient_name;
-    //console.log(prtContent);
-     //document.body.innerHTML = originalContents;
-     print();
-     $(".heading").show();
-     $("#print_list").show();
-    }
+    print();
+    $(".heading").show();
+    $("#print_list").show();
+    jQuery(".non_printable").show();
+    jQuery(".printable").hide();
+}
 
 function buildGroceryList(grocery_list, planned_recipes)
 {
-    console.log(grocery_list);
-    console.log(planned_recipes);
+    var list_items_html = "";
+    for (var ingredient_name in grocery_list) {
+        if (grocery_list.hasOwnProperty(ingredient_name)) {
+            for (var ingredient_measurement in grocery_list[ingredient_name]){
+                if (grocery_list[ingredient_name].hasOwnProperty(ingredient_measurement)) {
+                    ingredient_amount = grocery_list[ingredient_name][ingredient_measurement];
+                    list_items_html += "<div class='form-group'>";
+                    list_items_html += "<label class='col-sm-6 control-label'>" + ingredient_name + " (" + ingredient_measurement + ")</label>";
+                    list_items_html += "<div class='col-sm-2'>";
+                    list_items_html += "<input type='number' class='form-control' value='" + ingredient_amount + "' step='1' min='0' />";
+                    list_items_html += "</div>";
+                    list_items_html += "<div class='col-sm-2 non_printable'>";
+                    list_items_html += "<button class='delete non_printable'>x</button>";
+                    list_items_html += "<div class='check_del'>Remove?";
+                    list_items_html += "<button class='yes'>Yes</button>";
+                    list_items_html += "<button class='cancel'>Cancel</button>";
+                    list_items_html += "</div>";
+                    list_items_html += "</div>";
+                    list_items_html += "</div>";
+                    break;
+                }
+            }
+        }
+    }
+    jQuery("#grocery_list_items").html(list_items_html);
+    $(".check_del").hide();
+
+    list_items_html = "";
+    for (var key in planned_recipes) {
+        if (planned_recipes.hasOwnProperty(key)) {
+            list_items_html += "<li><span class='printable' style='display:none;'>" + planned_recipes[key]['recipe']['name'] + "</span><a class='non_printable' href='recipe_display.html?recipe_id=" + planned_recipes[key]['id'] + "'>" + planned_recipes[key]['recipe']['name'] + "</a></li>";
+        }
+    }
+    jQuery("#meal_list_items").html(list_items_html);
 }
 
 
@@ -66,7 +97,8 @@ function getGroceries(callback)
             var planned_recipe_ids = {};
             for (var key in planned_recipes_objects) {
                 if (planned_recipes_objects.hasOwnProperty(key)) {
-                    if (planned_recipes_objects[key]['date'] >= today.getTime()) {
+                    var planned_recipe_date = new Date(planned_recipes_objects[key]['date']);
+                    if (planned_recipe_date.getTime() >= today.getTime()) {
                         for (var temp_key in planned_recipes_objects[key]['recipe_ids']) {
                             if (planned_recipes_objects[key]['recipe_ids'].hasOwnProperty(temp_key)) {
                                 recipe_id = planned_recipes_objects[key]['recipe_ids'][temp_key];
@@ -97,6 +129,7 @@ function getGroceries(callback)
                         }
                     }
                 }
+                buildGroceryList(grocery_list, planned_recipes);
             });
             if (callback == undefined) {
                 callback = "getGroceriesResult";
@@ -113,4 +146,3 @@ function getGroceriesResult(grocery_list, planned_recipes)
     console.log(grocery_list);
     console.log(planned_recipes);
 }
-
